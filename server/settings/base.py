d7a0352e43f0480bba37bca4fc47dfce817ffe28
Seobi Oh <seobioh@gmail.com>
2026-01-29 16:20:06 +0900

    chore(settings): remove django_crontab from INSTALLED_APPS
    - Remove unused crontab
    - Use Celery Beat for scheduled tasks instead

--- server/settings/base.py
+++ server/settings/base.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',     # JWT
     'corsheaders',                  # CORS
-    'django_crontab',               # Crontab
     'drf_spectacular',              # Swagger
 
     # My apps
